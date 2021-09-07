4548cb5a95934987e2b5854a121ed61a5e79d1b4
Roudet <Celine.Roudet@u-bourgogne.fr>
2021-09-07 15:22:35 +0200

    ajout Fruit

---
+++ Ananas.java
@@ -0,0 +1,66 @@
+/**
+ *
+ * @author roudet
+ */
+package test;
+
+
+public class Ananas implements Fruit{
+    private double prix;
+    private String origine;
+
+    public Ananas()
+    {
+        this.prix = 0.5;  //prix en euros
+        this.origine="Espagne";
+    }
+
+    public Ananas(double prix, String origine)
+    {
+    	if(prix < 0)
+    	    this.prix = -prix;  //une solution possible pour interdire les prix negatifs
+    	else
+    	    this.prix = prix;
+
+    	if(origine.equals(""))
+                this.origine = "Finlande";  //Espagne par défaut
+    	else
+                this.origine = origine;
+    }
+
+    public double getPrix(){
+	   return prix;
+    }
+
+    public void setPrix(double prix){
+	   this.prix=prix;
+    }
+
+    public String getOrigine(){
+	   return origine;
+    }
+
+    public void setOrigine(String country){
+	     this.origine=country;
+    }
+
+    @Override
+    public String toString(){
+        return "Ananas de " + origine + " a " + prix + " euros";
+    }
+
+    @Override
+    public boolean equals(Object o){  //predicat pour tester si 2 Ananass sont equivalentes
+        if(o != null && getClass() == o.getClass()){
+            Ananas or = (Ananas) o;
+            return (prix == or.prix && origine.equals(or.origine));
+        }
+        return false;
+    }
+
+    public boolean isSeedless() {  //predicat indiquant qu'une Ananas a des pepins
+        return false;
+    }
+
+
+}
