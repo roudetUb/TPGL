10b4cf5b6c8e4d33f6b457925b4fcfc6c9cb6471
Roudet <Celine.Roudet@u-bourgogne.fr>
2021-09-08 11:59:12 +0200

    ajout Framboise

---
+++ Framboise.java
@@ -0,0 +1,74 @@
+package fr.ufrsciencestech.paniertp1.model;
+
+/**
+ *
+ * @author bobbyred
+ */
+public class Framboise implements Fruit {
+    private double prix;
+    private String origine;
+	
+    public Framboise() 
+    {
+        this.prix = 0.5;  //prix en euros
+        this.origine="Espagne";
+    }
+    
+    public Framboise(double prix, String origine) 
+    {
+	if(prix < 0)
+	    this.prix = -prix;  //une solution possible pour interdire les prix negatifs
+	else
+	    this.prix = prix;
+
+	if(origine.equals(""))
+            this.origine = "Espagne";  //Espagne par défaut
+	else
+            this.origine = origine;   
+    }
+
+    public double getPrix(){
+	return prix;
+    }
+
+    public void setPrix(double prix){
+	this.prix=prix;
+    }
+
+    public String getOrigine(){
+	return origine;
+    }
+ 
+    public void setOrigine(String country){
+	this.origine=origine;
+    }
+
+    @Override
+    public String toString(){
+        return "Framboise [de " + origine + " a " + prix + " €]";
+    }
+
+    @Override
+    public boolean equals(Object o){  //predicat pour tester si 2 oranges sont equivalentes
+        if(o != null && getClass() == o.getClass()){
+            Framboise or = (Framboise) o;
+            return (prix == or.prix && origine.equals(or.origine));
+        }
+        return false;
+    }
+
+    public boolean isSeedless() {  //predicat indiquant qu'une framboise n'a pas de pepins
+        return true;
+    }
+
+
+    public static void main (String[] args){
+        //Ecrire ici vos tests
+	System.out.println("premier test Framboise");
+	Framboise framboise = new Framboise();
+	System.out.println(framboise);
+	framboise.setOrigine("Maroc");
+	framboise.setPrix(20);
+	System.out.println(framboise);
+   }
+}
